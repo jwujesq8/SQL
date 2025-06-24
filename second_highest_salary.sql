b2c38646175cb6fe6854c3bfa20e0b32710ea98a
jwujesq8 <zhukovskaja.elena@gmail.com>
2025-06-24 22:02:53 +0200

    sql - get the second highest salary

---
+++ second_highest_salary.sql
@@ -0,0 +1,17 @@
+with distinct_salaries_cte as (
+    select distinct e.salary
+    from Employee e
+)
+select 
+    case
+        when (
+            select count(*)
+            from distinct_salaries_cte
+            ) < 2 then NULL
+        else (
+            select * 
+            FROM distinct_salaries_cte
+            order by salary desc
+            limit 1 OFFSET 1
+            )
+    end as SecondHighestSalary;
